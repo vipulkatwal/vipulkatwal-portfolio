63e7cc639b3b4194048a6880da5a859185609e2f
Vipul Katwal <vipulkatwal17@gmail.com>
2024-11-01 18:42:58 +0530

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -111,7 +111,7 @@ export const HeroSection = () => {
 
         <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4 relative z-20">
           <a
-            href="https://your-resume-link.com"
+            href="https://drive.google.com/file/d/1At3ucDvRpoDyDTjZpP1zTbjDtabNxq-W/view?usp=drivesdk"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl bg-gray-900"
